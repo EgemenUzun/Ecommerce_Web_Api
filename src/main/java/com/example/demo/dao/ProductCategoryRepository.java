2e0e3aecbe9c3ae3414c0f526cbe61df90402db1
Egemen <avniegemen1243@gmail.com>
2023-06-20 11:14:31 +0300

    Database Repositories

---
+++ src/main/java/com/example/demo/dao/ProductCategoryRepository.java
@@ -0,0 +1,10 @@
+package com.example.demo.dao;
+
+import com.example.demo.entity.ProductCategory;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource ;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+@RepositoryRestResource(collectionResourceRel = "productCategory",path ="  -category")
+public interface ProductCategoryRepository  extends JpaRepository<ProductCategory,Long> {
+}
