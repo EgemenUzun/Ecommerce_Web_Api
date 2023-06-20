bea18ada02c6240b4f134531d59ab41350715016
Egemen <avniegemen1243@gmail.com>
2023-06-20 10:04:06 +0300

    creating entities

---
+++ src/main/java/com/example/demo/entity/Country.java
@@ -0,0 +1,25 @@
+package com.example.demo.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.List;
+
+@Entity
+@Table(name="country")
+@Data
+public class Country {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private  int id;
+    @Column(name = "code")
+    private  String code;
+    @Column(name = "name")
+    private  String  name;
+    //RelationShip for database
+    @OneToMany(mappedBy = "country")
+    @JsonIgnore
+    private List<State> states;
+}
