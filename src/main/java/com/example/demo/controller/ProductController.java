a4d08b38112a3ffb6bcedec0c223f9ce4572c631
Egemen <avniegemen1243@gmail.com>
2023-06-21 22:42:37 +0300

    security configuration and Rabbitmq integration

--- src/main/java/com/example/demo/controller/ProductController.java
+++ src/main/java/com/example/demo/controller/ProductController.java
@@ -1,5 +1,6 @@
 package com.example.demo.controller;
 
+import com.example.demo.RabbitMQ.QueueSender;
 import com.example.demo.entity.Product;
 import com.example.demo.service.ProductService;
 import org.springframework.http.HttpStatus;
@@ -10,13 +11,16 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/api/products")
 public class ProductController {
     private final ProductService productService;
+    private final QueueSender queueSender;
 
-    public ProductController(ProductService productService) {
+    public ProductController(ProductService productService, QueueSender queueSender) {
         this.productService = productService;
+        this.queueSender = queueSender;
     }
     @PostMapping
     public ResponseEntity<Product> createProduct(@RequestBody Product product) {
         Product createdProduct = productService.createProduct(product);
+        queueSender.send("Product Created. Product id is:"+createdProduct.getId());
         return new ResponseEntity<>(createdProduct, HttpStatus.CREATED);
     }
     @GetMapping("/{id}")
