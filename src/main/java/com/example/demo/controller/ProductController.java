478882dfe9c97c90e8b75a5961b1250a58411e54
Egemen <avniegemen1243@gmail.com>
2023-06-22 17:29:46 +0300

    added product discount

--- src/main/java/com/example/demo/controller/ProductController.java
+++ src/main/java/com/example/demo/controller/ProductController.java
@@ -5,8 +5,15 @@ import com.example.demo.entity.Product;
 import com.example.demo.service.ProductService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.web.bind.annotation.*;
 
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.Random;
+
+@EnableScheduling
 @RestController
 @RequestMapping("/api/products")
 public class ProductController {
@@ -27,4 +34,27 @@ public class ProductController {
     public Product getProduct(@PathVariable Long id) {
         return productService.getProduct(id);
     }
+    Product randomProduct;
+    @GetMapping("/discount")
+    public Product discountProduct()
+    {
+        return randomProduct;
+    }
+
+    @Scheduled(fixedRate = 60000)//120000
+    public Product Discount()
+    {
+        List<Product> products = productService.getAllProducts();
+        Random random = new Random();
+        int randomIndex = random.nextInt(100);
+        randomProduct = products.get(randomIndex);
+        double value = randomProduct.getUnitPrice().doubleValue();
+        value -= value* 0.3;
+        BigDecimal result = BigDecimal.valueOf(value);
+        randomProduct.setUnitPrice(result);
+        System.out.println(randomProduct.getId()+"/"+randomProduct.getName());
+        queueSender.send("New discount arrived check the mambers page for the opportunity");
+        return randomProduct;
+    }
+
 }
