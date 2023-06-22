f170fa5a9b9fb0d56cb25eb693386a9e68de2c1a
Egemen <avniegemen1243@gmail.com>
2023-06-22 10:37:23 +0300

    RabbitMq Configuration

--- src/main/java/com/example/demo/RabbitMQ/QueueSender.java
+++ src/main/java/com/example/demo/RabbitMQ/QueueSender.java
@@ -1,19 +1,25 @@
 package com.example.demo.RabbitMQ;
-import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.Exchange;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
 public class QueueSender {
 
-    @Autowired
-    private RabbitTemplate rabbitTemplate;
+    private final RabbitTemplate rabbitTemplate;
+    private final Exchange exchange;
+    private final String routingKey;
 
     @Autowired
-    private Queue queue;
+    public QueueSender(RabbitTemplate rabbitTemplate, Exchange exchange, @Value("${routing.key}") String routingKey) {
+        this.rabbitTemplate = rabbitTemplate;
+        this.exchange = exchange;
+        this.routingKey = routingKey;
+    }
 
-    public void send(String order) {
-        rabbitTemplate.convertAndSend(this.queue.getName(), order);
+    public void send(String message) {
+        rabbitTemplate.convertAndSend(exchange.getName(), routingKey, message);
     }
 }
--- src/main/java/com/example/demo/RabbitMQ/SenderConfig.java
+++ src/main/java/com/example/demo/RabbitMQ/SenderConfig.java
@@ -1,5 +1,8 @@
 package com.example.demo.RabbitMQ;
 
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.FanoutExchange;
 import org.springframework.amqp.core.Queue;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -8,12 +11,27 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class SenderConfig {
 
+    @Value("${exchange.name}")
+    private String exchangeName;
+
+    @Value("${routing.key}")
+    private String routingKey;
+
     @Value("${queue.name}")
-    private String message;
+    private String queueName;
 
     @Bean
     public Queue queue() {
-        return new Queue(message, true);
+        return new Queue(queueName, true);
     }
 
+    @Bean
+    public FanoutExchange exchange() {
+        return new FanoutExchange(exchangeName);
+    }
+
+    @Bean
+    public Binding binding(FanoutExchange exchange, Queue queue) {
+        return BindingBuilder.bind(queue).to(exchange);
+    }
 }
